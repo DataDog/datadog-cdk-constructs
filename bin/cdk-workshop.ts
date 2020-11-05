835e4fb8c1ac43d06ca881b2c485540b6c955431
Meghna Banerjee <meghna.banerjee@datadoghq.com>
2020-11-05 15:15:59 -0800

    Initial commit

---
+++ bin/cdk-workshop.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from '@aws-cdk/core';
+import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
+
+const app = new cdk.App();
+new CdkWorkshopStack(app, 'CdkWorkshopStack');
