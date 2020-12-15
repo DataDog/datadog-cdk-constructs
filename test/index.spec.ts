c8dc86a786ec37fe80e6529b1aee87d33759b45e
Meghna Banerjee <meghna.banerjee@datadoghq.com>
2020-12-15 12:39:06 -0800

    add index test for python

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,7 +2,7 @@ import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
 import "@aws-cdk/assert/jest";
 import { Datadog } from "../lib/index";
-import { JS_HANDLER_WITH_LAYERS, DD_HANDLER_ENV_VAR } from "../lib/redirect";
+import { JS_HANDLER_WITH_LAYERS, DD_HANDLER_ENV_VAR, PYTHON_HANDLER } from "../lib/redirect";
 describe("applyLayers", () => {
   it("if addLayers is not given, layer is added", () => {
     const app = new cdk.App();
@@ -23,6 +23,34 @@ describe("applyLayers", () => {
     });
     expect(stack).toHaveResource("AWS::Lambda::Function", {
       Handler: `${JS_HANDLER_WITH_LAYERS}`,
+    });
+    expect(stack).toHaveResource("AWS::Lambda::Function", {
+      Environment: {
+        Variables: {
+          [DD_HANDLER_ENV_VAR]: "hello.handler",
+        },
+      },
+    });
+  });
+  it("layer is added for python", () => {
+    const app = new cdk.App();
+    const stack = new cdk.Stack(app, "stack", {
+      env: {
+        region: "us-west-2",
+      },
+    });
+    const hello = new lambda.Function(stack, "HelloHandler", {
+      runtime: lambda.Runtime.PYTHON_3_6,
+      code: lambda.Code.inline("test"),
+      handler: "hello.handler",
+    });
+    new Datadog(stack, "Datadog", {
+      lambdaFunctions: [hello],
+      nodeLayerVersion: 39,
+      pythonLayerVersion: 24,
+    });
+    expect(stack).toHaveResource("AWS::Lambda::Function", {
+      Handler: `${PYTHON_HANDLER}`,
     });
     expect(stack).toHaveResource("AWS::Lambda::Function", {
       Environment: {
