842ce16a93e81079678ccf5ec4f3da58ea19b46e
Andrew Rodriguez <andrew.rodriguez@datadoghq.com>
2021-04-20 10:29:01 -0700

    Fix typo and remove unecessary parameters/imports from lambdaDestination

--- src/lambdaDestination.ts
+++ src/lambdaDestination.ts
@@ -1,17 +1,14 @@
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as logs from "@aws-cdk/aws-logs";
-import { Construct } from "@aws-cdk/core";
 
 /**
- * Use a Lamda Function as the destination for a log subscription.
+ * Use a Lambda Function as the destination for a log subscription.
  * Using this class temporarily until https://github.com/aws/aws-cdk/pull/14222 is merged and released.
  */
 export class LambdaDestination implements logs.ILogSubscriptionDestination {
   constructor(private readonly fn: lambda.IFunction) {}
 
-  public bind(scope: Construct, logGroup: logs.ILogGroup): logs.LogSubscriptionDestinationConfig {
-    scope.toString;
-    logGroup.logGroupName; //get around not using the params
+  public bind(): logs.LogSubscriptionDestinationConfig {
     return { arn: this.fn.functionArn };
   }
 }
