b224ae923927f5c7037cee0daac78bd6199025a5
Andrew Rodriguez <andrew.rodriguez@datadoghq.com>
2021-04-22 16:57:08 -0700

    Add dd_cdk_construct tag to all lambda functions deployed via the DD CDK Construct, add test, mess with eslint to allow require imports

--- .projenrc.js
+++ .projenrc.js
@@ -68,6 +68,8 @@ eslintConfig.addOverride("extends", [
 ]);
 
 eslintConfig.addOverride("rules", {
+  "@typescript-eslint/no-require-imports": "off",
+  "@typescript-eslint/no-var-requires": "off",
   "@typescript-eslint/no-explicit-any": "off",
   "@typescript-eslint/no-empty-interface": "off",
   "@typescript-eslint/explicit-module-boundary-types": "off",
