b67e691eab1be67213140155ed62f3274186dffe
Andrew Rodriguez <andrew.rodriguez@datadoghq.com>
2021-04-28 16:48:53 -0700

    Strengthen version regex

--- scripts/publish_prod.sh
+++ scripts/publish_prod.sh
@@ -23,7 +23,7 @@ CURRENT_VERSION=$(node -pe "require('./package.json').version")
 if [ -z "$1" ]; then
     echo "Must specify a desired version number"
     exit 1
-elif [[ ! $1 =~ [0-9]+\.[0-9]+\.[0-9]+ ]]; then
+elif [[ ! $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     echo "Must use a semantic version, e.g., 3.1.4"
     exit 1
 else
