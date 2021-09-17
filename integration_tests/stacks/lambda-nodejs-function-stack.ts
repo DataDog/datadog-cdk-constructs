da16e7593b5fb518aaf69ae723ff98277e0901eb
Tony Saavedra <ajsaavedrax@gmail.com>
2021-09-17 11:18:00 -0700

    Datadog CDK Construct Integration tests (#57)
    * Added base integration test for an aws-lambda construct
    
    * Adding in required tsconfig file
    
    * updates to integration test
    
    * Cleaning up and adding the NodejsFunction stack to integration test
    
    * Added comment
    
    * Normalizing s3 key timestamps
    
    * Normalizing output
    
    * Adding github workflow to run integration tests
    
    * Adding cdk cli
    
    * Test
    
    * Updating lambda entry
    
    * Moving snapshots to own directory
    
    * Removing output to stdout
    
    * Refactoring integration test
    
    * Updating stack names
    
    * Updating test output
    
    * Updating variable casing

---
+++ integration_tests/stacks/lambda-nodejs-function-stack.ts
@@ -0,0 +1,40 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed
+ * under the Apache License Version 2.0.
+ *
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2021 Datadog, Inc.
+ */
+
+import * as lambda from "@aws-cdk/aws-lambda";
+import { NodejsFunction } from "@aws-cdk/aws-lambda-nodejs";
+import * as cdk from "@aws-cdk/core";
+import { Datadog } from "../../src/index";
+
+export class ExampleStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const lambdaNodejsFunction = new NodejsFunction(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      entry: __dirname + "/../lambda/example-lambda.js",
+      handler: "handler",
+    });
+
+    const datadogCDK = new Datadog(this, "Datadog", {
+      nodeLayerVersion: 62,
+      extensionLayerVersion: 10,
+      enableDatadogTracing: true,
+      flushMetricsToLogs: true,
+      apiKey: "1234",
+      site: "datadoghq.com",
+    });
+    datadogCDK.addLambdaFunctions([lambdaNodejsFunction]);
+  }
+}
+
+const app = new cdk.App();
+const env = { account: "601427279990", region: "sa-east-1" };
+const stack = new ExampleStack(app, "lambda-nodejs-function-stack", { env: env });
+console.log("Stack name: " + stack.stackName);
+app.synth();
