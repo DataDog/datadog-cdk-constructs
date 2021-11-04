c50c006799a99a487248313f57cd618060612a3d
Nick Hinsch <nick.hinsch@datadoghq.com>
2021-11-04 16:54:46 -0400

    Rm esbuild

--- .projenrc.js
+++ .projenrc.js
@@ -33,7 +33,7 @@ const project = new AwsCdkConstructLibrary({
     "@aws-cdk/core",
     "@aws-cdk/aws-apigateway",
   ],
-  devDeps: ["esbuild", "ts-node", "prettier", "eslint-config-prettier", "eslint-plugin-prettier"],
+  devDeps: ["ts-node", "prettier", "eslint-config-prettier", "eslint-plugin-prettier"],
   gitignore: [
     "*.js",
     "!jest.config.js",
