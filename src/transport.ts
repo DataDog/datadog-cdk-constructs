b4f0bcbc6f5a9400a3f54b1f25ee9e4cb77b9c6c
Jordan Gonzalez <30836115+duncanista@users.noreply.github.com>
2021-11-05 13:38:01 -0400

    Add support for Secrets Manager (#72)
    * add support for secrets manager
    
    * restrict usage of secret arn when flush metrics are false
    
    * ignore apiKeySecretArn when using synchronous metrics in node
    
    * fix issues with isSendingSynchronousMetrics and add tests for edge cases in python and node
    
    * update debug log to error message when using sync metrics and node runtime
    
    * paraphrase error
    
    * change if statement to be cleaner
    
    * add documentation
    
    * move isNode and isSendingSync... one level down

--- src/transport.ts
+++ src/transport.ts
@@ -8,8 +8,10 @@
 
 import * as lambda from "@aws-cdk/aws-lambda";
 import log from "loglevel";
+import { runtimeLookup, RuntimeType } from ".";
 
 export const API_KEY_ENV_VAR = "DD_API_KEY";
+export const API_KEY_SECRET_ARN_ENV_VAR = "DD_API_KEY_SECRET_ARN";
 export const KMS_API_KEY_ENV_VAR = "DD_KMS_API_KEY";
 export const SITE_URL_ENV_VAR = "DD_SITE";
 export const FLUSH_METRICS_TO_LOGS_ENV_VAR = "DD_FLUSH_TO_LOG";
@@ -24,6 +26,7 @@ export class Transport {
   flushMetricsToLogs: boolean;
   site: string;
   apiKey?: string;
+  apiKeySecretArn?: string;
   apiKmsKey?: string;
   extensionLayerVersion?: number;
 
@@ -31,6 +34,7 @@ export class Transport {
     flushMetricsToLogs?: boolean,
     site?: string,
     apiKey?: string,
+    apiKeySecretArn?: string,
     apiKmsKey?: string,
     extensionLayerVersion?: number,
   ) {
@@ -56,6 +60,7 @@ export class Transport {
     }
 
     this.apiKey = apiKey;
+    this.apiKeySecretArn = apiKeySecretArn;
     this.apiKmsKey = apiKmsKey;
   }
 
@@ -68,6 +73,16 @@ export class Transport {
       }
       if (this.apiKey !== undefined) {
         lam.addEnvironment(API_KEY_ENV_VAR, this.apiKey);
+      }
+      if (this.apiKeySecretArn !== undefined) {
+        const isNode = runtimeLookup[lam.runtime.name] === RuntimeType.NODE;
+        const isSendingSynchronousMetrics = this.extensionLayerVersion === undefined && !this.flushMetricsToLogs;
+        if (isSendingSynchronousMetrics && isNode) {
+          throw new Error(
+            `\`apiKeySecretArn\` is not supported for Node runtimes when using Synchronous Metrics. Use either \`apiKey\` or \`apiKmsKey\`.`,
+          );
+        }
+        lam.addEnvironment(API_KEY_SECRET_ARN_ENV_VAR, this.apiKeySecretArn);
       }
       if (this.apiKmsKey !== undefined) {
         lam.addEnvironment(KMS_API_KEY_ENV_VAR, this.apiKmsKey);
