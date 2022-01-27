dfa8b17375b046904795e3c13893ccbd6b0b3497
David Lee <david.lee@datadoghq.com>
2022-01-27 13:58:45 -0500

    Require Node14+ for v2

--- v2/.projenrc.js
+++ v2/.projenrc.js
@@ -11,6 +11,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   repositoryUrl: "https://github.com/DataDog/datadog-cdk-constructs",
 
   packageManager: javascript.NodePackageManager.YARN,
+  minNodeVersion: "14.15.0",
 
   jsiiFqn: "projen.AwsCdkConstructLibrary",
   defaultReleaseBranch: "main",
