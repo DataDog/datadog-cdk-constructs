638f96f5c1f1eaf3704d61dd039cb44bcffd710f
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2023-05-15 15:07:34 -0400

    redirect only when `redirectHandler` is `true`

--- v1/src/datadog.ts
+++ v1/src/datadog.ts
@@ -71,7 +71,10 @@ export class Datadog extends cdk.Construct {
           this.props.extensionLayerVersion,
         );
       }
-      redirectHandlers(lambdaFunctions, baseProps.addLayers);
+
+      if (baseProps.redirectHandler) {
+        redirectHandlers(lambdaFunctions, baseProps.addLayers);
+      }
 
       if (this.props.forwarderArn !== undefined) {
         if (this.props.extensionLayerVersion !== undefined) {
