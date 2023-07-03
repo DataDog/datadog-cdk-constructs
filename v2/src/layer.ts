871183da3bc5529d14dc0102821e7b986fb33ef8
Stephen Firincieli <stephen.firrincieli@datadoghq.com>
2023-07-03 17:02:26 -0400

    Fix comment

--- v2/src/layer.ts
+++ v2/src/layer.ts
@@ -42,8 +42,6 @@ export function applyLayers(
       lam.architecture.dockerPlatform === Architecture.ARM_64.dockerPlatform;
     const isNode = lambdaRuntimeType === RuntimeType.NODE;
 
-    // console.log({ lam });
-
     if (lambdaRuntimeType === undefined || lambdaRuntimeType === RuntimeType.UNSUPPORTED) {
       log.debug(`Unsupported runtime: ${runtime}`);
       return;
