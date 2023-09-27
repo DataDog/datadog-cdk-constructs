074ae8bd68dc59b7137ea3f8b89444ceb30da331
Romain Marcadier <romain.marcadier@datadoghq.com>
2023-09-27 10:36:31 +0200

    remove use of snake case

--- examples/typescript-cdk-v2/lib/typescript_v2-stack.ts
+++ examples/typescript-cdk-v2/lib/typescript_v2-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import { Function } from "aws-cdk-lib/aws-lambda";
-import * as aws_apigateway from "aws-cdk-lib/aws-apigateway";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 import { Datadog } from "datadog-cdk-constructs-v2";
 import { Stack, StackProps } from "aws-cdk-lib";
@@ -30,14 +30,14 @@ export class TypescriptV2Stack extends Stack {
       handler: "hello_py.lambda_handler",
     });
 
-    const apig = new aws_apigateway.RestApi(this, "RestAPI").root;
+    const apig = new apigateway.RestApi(this, "RestAPI").root;
     apig.addResource('node').addProxy({
       anyMethod: true,
-      defaultIntegration: new aws_apigateway.LambdaIntegration(helloNode),
+      defaultIntegration: new apigateway.LambdaIntegration(helloNode),
     });
     apig.addResource('python').addProxy({
       anyMethod: true,
-      defaultIntegration: new aws_apigateway.LambdaIntegration(helloPython),
+      defaultIntegration: new apigateway.LambdaIntegration(helloPython),
     });
 
     console.log("Instrumenting with Datadog");
