f99020d41347c5e52adff9e65a921286848b541c
Rey Abolofia <rey.abolofia@datadoghq.com>
2025-05-05 08:58:52 -0700

    Create DatadogAPIGatewayV2ParameterMapping object.

--- src/apigateway/parameters.ts
+++ src/apigateway/parameters.ts
@@ -1,3 +1,5 @@
+import * as apigatewayv2 from 'aws-cdk-lib/aws-apigatewayv2';
+
 export const DatadogAPIGatewayRequestParameters = {
   "integration.request.header.x-dd-proxy": "'aws-apigateway'",
   "integration.request.header.x-dd-proxy-request-time-ms": "context.requestTimeEpoch",
@@ -6,3 +8,13 @@ export const DatadogAPIGatewayRequestParameters = {
   "integration.request.header.x-dd-proxy-path": "context.path",
   "integration.request.header.x-dd-proxy-stage": "context.stage",
 }
+
+// TODO: make this a function that takes an optional existing parameter mapping
+// and returns a new one with the same values
+export DatadogAPIGatewayV2ParameterMapping = new apigatewayv2.ParameterMapping()
+  .appendHeader('x-dd-proxy', apigatewayv2.MappingValue.custom("'aws-apigateway'"))
+  .appendHeader('x-dd-proxy-request-time-ms', apigatewayv2.MappingValue.custom('${context.requestTimeEpoch}000'))
+  .appendHeader('x-dd-proxy-domain-name', apigatewayv2.MappingValue.custom('$context.domainName'))
+  .appendHeader('x-dd-proxy-httpmethod', apigatewayv2.MappingValue.custom('$context.httpMethod'))
+  .appendHeader('x-dd-proxy-path', apigatewayv2.MappingValue.custom('$context.path'))
+  .appendHeader('x-dd-proxy-stage', apigatewayv2.MappingValue.custom('$context.stage'))
