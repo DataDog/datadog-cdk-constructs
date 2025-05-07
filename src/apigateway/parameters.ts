eb8da7147698dda4dc4f4f4641ec4d524a769e1a
Rey Abolofia <rey.abolofia@datadoghq.com>
2025-05-06 18:03:05 -0700

    Convert v2 apigw parameter mappings to a function.

--- src/apigateway/parameters.ts
+++ src/apigateway/parameters.ts
@@ -1,6 +1,6 @@
 import * as apigatewayv2 from 'aws-cdk-lib/aws-apigatewayv2';
 
-export const DatadogAPIGatewayRequestParameters = {
+const DatadogAPIGatewayRequestParameters = {
   "integration.request.header.x-dd-proxy": "'aws-apigateway'",
   "integration.request.header.x-dd-proxy-request-time-ms": "context.requestTimeEpoch",
   "integration.request.header.x-dd-proxy-domain-name": "context.domainName",
@@ -9,12 +9,25 @@ export const DatadogAPIGatewayRequestParameters = {
   "integration.request.header.x-dd-proxy-stage": "context.stage",
 }
 
-// TODO: make this a function that takes an optional existing parameter mapping
-// and returns a new one with the same values
-export DatadogAPIGatewayV2ParameterMapping = new apigatewayv2.ParameterMapping()
-  .appendHeader('x-dd-proxy', apigatewayv2.MappingValue.custom("'aws-apigateway'"))
-  .appendHeader('x-dd-proxy-request-time-ms', apigatewayv2.MappingValue.custom('${context.requestTimeEpoch}000'))
-  .appendHeader('x-dd-proxy-domain-name', apigatewayv2.MappingValue.custom('$context.domainName'))
-  .appendHeader('x-dd-proxy-httpmethod', apigatewayv2.MappingValue.custom('$context.httpMethod'))
-  .appendHeader('x-dd-proxy-path', apigatewayv2.MappingValue.custom('$context.path'))
-  .appendHeader('x-dd-proxy-stage', apigatewayv2.MappingValue.custom('$context.stage'))
+function appendDatadogHeaders(
+  existingMapping?: apigatewayv2.ParameterMapping
+): apigatewayv2.ParameterMapping {
+  if (!existingMapping) {
+    existingMapping = new apigatewayv2.ParameterMapping();
+  }
+  return existingMapping
+    .appendHeader('x-dd-proxy', apigatewayv2.MappingValue.custom('aws-apigateway'))
+    .appendHeader('x-dd-proxy-request-time-ms', apigatewayv2.MappingValue.custom('${context.requestTimeEpoch}000'))
+    .appendHeader('x-dd-proxy-domain-name', apigatewayv2.MappingValue.custom('$context.domainName'))
+    .appendHeader('x-dd-proxy-httpmethod', apigatewayv2.MappingValue.custom('$context.httpMethod'))
+    .appendHeader('x-dd-proxy-path', apigatewayv2.MappingValue.custom('$context.path'))
+    .appendHeader('x-dd-proxy-stage', apigatewayv2.MappingValue.custom('$context.stage'));
+}
+
+const DatadogAPIGatewayV2ParameterMapping = appendDatadogHeaders();
+
+export {
+  DatadogAPIGatewayRequestParameters,
+  DatadogAPIGatewayV2ParameterMapping,
+  appendDatadogHeaders,
+};
