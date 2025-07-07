ac0eb8d6f2fca634574803015e58377bb874ad0a
Radia Wong <radia.wong@datadoghq.com>
2025-07-07 12:50:45 -0400

    addressed git override tests where on some local machines the git respository check was too strict and failed

--- test/datadog-lambda.spec.ts
+++ test/datadog-lambda.spec.ts
@@ -805,13 +805,13 @@ describe("overrideGitMetadata", () => {
     datadogLambda.overrideGitMetadata("fake-sha");
     datadogLambda.addLambdaFunctions([goodbye], stack);
 
-    [hello, goodbye].forEach((f) => {
-      expect((<any>f).environment[DD_TAGS].value.split(",")).toEqual(
-        expect.arrayContaining([
-          "git.commit.sha:fake-sha",
-          "git.repository_url:github.com/DataDog/datadog-cdk-constructs",
-        ]),
-      );
+    [hello, goodbye].forEach((f) => { //check if each expected tag is present in the lambda function's tags
+      const expectedTags = ["git.commit.sha:fake-sha", 
+                            "git.repository_url:github.com/DataDog/datadog-cdk-constructs"];
+      const fTags = (<any>f).environment[DD_TAGS].value.split(",");
+      expectedTags.forEach((tag) => {
+        expect(fTags.some((item: string) => item.includes(tag))).toEqual(true);
+      });
     });
   });
 
@@ -836,9 +836,9 @@ describe("overrideGitMetadata", () => {
     expect(
       (<any>hello).environment[DD_TAGS].value.split(",").some((item: string) => item.includes("git.commit.sha")),
     ).toEqual(true);
-    expect((<any>hello).environment[DD_TAGS].value.split(",")).toEqual(
-      expect.arrayContaining(["git.repository_url:github.com/DataDog/datadog-cdk-constructs"]),
-    );
+    expect(
+      (<any>hello).environment[DD_TAGS].value.split(",").some((item: string) => item.includes("git.repository_url:github.com/DataDog/datadog-cdk-constructs")),
+    ).toEqual(true);
   });
 
   it("overrides using context", () => {
