7df25b20b897e9dd76dd12f6b6f0ef77da39e507
Ava Silver <ava.silver@datadoghq.com>
2025-08-05 11:46:49 -0400

    [SVLS-7337] add github integration

--- scripts/lambda_tf/update_version.py
+++ scripts/lambda_tf/update_version.py
@@ -5,6 +5,11 @@ import sys
 from subprocess import run
 
 os.chdir(os.path.expanduser("~/dd/terraform-aws-lambda-datadog"))
+if run(["git", "status", "--porcelain"], capture_output=True, text=True).stdout.strip():
+    print("You have uncommitted changes. Please commit or stash them before proceeding.")
+    sys.exit(1)
+run(["git", "switch", "main"], check=True)
+run(["git", "pull"], check=True)
 
 with open("main.tf") as f:
     main = f.read()
@@ -36,14 +41,10 @@ with open("README.md", "r+") as f:
 
 create_pr = input("Create a PR with these changes? (y/n): ").strip().lower()
 if create_pr == "y":
-    run("git checkout main", check=True)
-    if run(["git", "status", "--porcelain"], capture_output=True, text=True).stdout.strip():
-        print("You have uncommitted changes. Please commit or stash them before proceeding.")
-        sys.exit(1)
-    run(f"git checkout -b '{os.environ['USER']}/update-version-{new_version}'", check=True)
-    run("git add main.tf README.md", check=True)
-    run(f"git commit -m 'chore: update version to {new_version}'", check=True)
-    run("git push origin HEAD", check=True)
-    run("gh pr create --fill", check=True)
+    run(["git", "add", "main.tf", "README.md"], check=True)
+    run(["git", "checkout", "-b", f"{os.environ['USER']}/update-version-{new_version}"], check=True)
+    run(["git", "commit", "-m", f"chore: update version to {new_version}"], check=True)
+    run(["git", "push", "origin", "HEAD"], check=True)
+    run(["gh", "pr", "create", "--fill"], check=True)
 
 print("Done ✅")
