ef5a551bef3b9e6e440f5c8ce1c1ee2e85977ab1
Alex Angelillo <alex.angelillo@datadoghq.com>
2025-08-27 10:48:15 -0400

    tmp

--- utilities/login-npm.sh
+++ utilities/login-npm.sh
@@ -51,22 +51,23 @@ fi
 NPM_TOKEN_VAL=$(VAULT_ADDR=$VAULT_ADDR vault kv get -field="$NPM_TOKEN" "$VAULT_PREFIX")
 
 
-yarn config set registry https://registry.npmjs.org/
-yarn config set _authToken $NPM_TOKEN_VAL --silent
+# yarn config set registry https://registry.npmjs.org/
+# yarn config set _authToken $NPM_TOKEN_VAL --silent
 
 
-echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN_VAL" >> ~/.npmrc
+# echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN_VAL" >> ~/.npmrc
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN_VAL" >> .npmrc
+echo "registry=https://registry.npmjs.org/" >> .npmrc
 
 echo "\"_authToken\" $NPM_TOKEN_VAL" >> .npmrc
 
-echo "registry=https://$CI_SERVER_HOST/api/v4/projects/$CI_PROJECT_ID/packages/npm/" >> ~/.npmrc
-echo "//$CI_SERVER_HOST/api/v4/projects/$CI_PROJECT_ID/packages/npm/:_authToken=$NPM_TOKEN_VAL" >> ~/.npmrc
+# echo "registry=https://$CI_SERVER_HOST/api/v4/projects/$CI_PROJECT_ID/packages/npm/" >> ~/.npmrc
+# echo "//$CI_SERVER_HOST/api/v4/projects/$CI_PROJECT_ID/packages/npm/:_authToken=$NPM_TOKEN_VAL" >> ~/.npmrc
 
-echo "registry=https://$CI_SERVER_HOST/api/v4/projects/$CI_PROJECT_ID/packages/npm/" >> .npmrc
-echo "//$CI_SERVER_HOST/api/v4/projects/$CI_PROJECT_ID/packages/npm/:_authToken=$NPM_TOKEN_VAL" >> .npmrc
+# echo "registry=https://$CI_SERVER_HOST/api/v4/projects/$CI_PROJECT_ID/packages/npm/" >> .npmrc
+# echo "//$CI_SERVER_HOST/api/v4/projects/$CI_PROJECT_ID/packages/npm/:_authToken=$NPM_TOKEN_VAL" >> .npmrc
 
-echo "//registry.npmjs.org/api/v4/projects/$CI_PROJECT_ID/packages/npm/:_authToken=$NPM_TOKEN_VAL" >> ~/.npmrc
-echo "//registry.npmjs.org/api/v4/projects/$CI_PROJECT_ID/packages/npm/:_authToken=$NPM_TOKEN_VAL" >> .npmrc
+# echo "//registry.npmjs.org/api/v4/projects/$CI_PROJECT_ID/packages/npm/:_authToken=$NPM_TOKEN_VAL" >> ~/.npmrc
+# echo "//registry.npmjs.org/api/v4/projects/$CI_PROJECT_ID/packages/npm/:_authToken=$NPM_TOKEN_VAL" >> .npmrc
 
 export NPM_TOKEN_VAL=$NPM_TOKEN_VAL
