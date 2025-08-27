45270a1eccc65cf5eddc733e9b4a0c1cc9aef3d7
Alex Angelillo <alex.angelillo@datadoghq.com>
2025-08-27 12:57:40 -0400

    tmp

--- datadog-cdk-constructs/release.sh
+++ datadog-cdk-constructs/release.sh
@@ -33,8 +33,10 @@ yarn build
 # JS Publishing
 JS_TARBALL_PATH=./dist/js/datadog-cdk-constructs-v2@$VERSION.jsii.tgz
 echo "Publishing JS package"
+yarn config get registry
 yarn publish $JS_TARBALL_PATH --new-version "$VERSION" --verbose
 echo "JS package published"
+
 
 # npm publish $JS_TARBALL_PATH --new-version "$VERSION" --verbose
 # NPM_TOKEN=$NPM_TOKEN_VAL NPM_REGISTRY=https://registry.npmjs.org/ npx -p publib@latest publib-npm
