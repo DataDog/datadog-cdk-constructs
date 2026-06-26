9d79322b6b10a61ac1beb23896836eb6accd60f8
Ava Silver <ava.silver@datadoghq.com>
2026-06-26 10:39:10 -0400

    chore: update year

--- src/ecs/fargate/utils.ts
+++ src/ecs/fargate/utils.ts
@@ -3,7 +3,7 @@
  * under the Apache License Version 2.0.
  *
  * This product includes software developed at Datadog (https://www.datadoghq.com/).
- * Copyright 2021 Datadog, Inc.
+ * Copyright 2026 Datadog, Inc.
  */
 
 import log from "loglevel";
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,7 +3,7 @@
  * under the Apache License Version 2.0.
  *
  * This product includes software developed at Datadog (https://www.datadoghq.com/).
- * Copyright 2021 Datadog, Inc.
+ * Copyright 2026 Datadog, Inc.
  */
 
 import * as lambda from "aws-cdk-lib/aws-lambda";
