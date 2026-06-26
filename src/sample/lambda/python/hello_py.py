62adf03c9dae277c128ef5044b2f0b232eb9d1f5
Ava Silver <ava.silver@datadoghq.com>
2026-06-26 13:32:11 -0400

    chore: update year (#624)
    * chore: update year
    
    * clean up
    
    * update range

--- src/sample/lambda/python/hello_py.py
+++ src/sample/lambda/python/hello_py.py
@@ -1,17 +1,12 @@
 # Unless explicitly stated otherwise all files in this repository are licensed
 # under the Apache License Version 2.0.
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
-# Copyright 2021 Datadog, Inc.
-
-import json
+# Copyright 2020-2026 Datadog, Inc.
 
 
 def handler(event, context):
-    # print('request: {}'.format(json.dumps(event)))
     return {
-        'statusCode': 200,
-        'headers': {
-            'Content-Type': 'text/plain'
-        },
-        'body': 'Hello, CDK! You have hit '
+        "statusCode": 200,
+        "headers": {"Content-Type": "text/plain"},
+        "body": "Hello, CDK! You have hit ",
     }
