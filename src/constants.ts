f5fb287a5afa83fb4f34828b57a10a5881a8a7fc
Ava Silver <ava.silver@datadoghq.com>
2026-08-25 15:14:39 -0400

    chore: prepare lambda layer catalog (#685)
    * chore: prepare lambda layer catalog
    
    * rename layer catalog
    
    * catalog runtime data

--- src/constants.ts
+++ src/constants.ts
@@ -7,6 +7,7 @@
  */
 
 import { DatadogAppSecMode } from "./interfaces";
+import { layerRuntimeCatalog } from "./layer-runtime-catalog";
 
 export const LAYER_PREFIX = "DatadogLayer";
 export const EXTENSION_LAYER_PREFIX = "DatadogExtension";
@@ -29,6 +30,15 @@ export enum RuntimeType {
   CUSTOM,
   UNSUPPORTED,
 }
+
+const runtimeTypeLookup: { [key: string]: RuntimeType } = {
+  DOTNET: RuntimeType.DOTNET,
+  NODE: RuntimeType.NODE,
+  PYTHON: RuntimeType.PYTHON,
+  JAVA: RuntimeType.JAVA,
+  RUBY: RuntimeType.RUBY,
+  CUSTOM: RuntimeType.CUSTOM,
+};
 
 export const DatadogLambdaDefaultProps = {
   addLayers: true,
@@ -58,70 +68,15 @@ export enum TagKeys {
   DD_TRACE_ENABLED = "DD_TRACE_ENABLED",
 }
 
-export const runtimeLookup: { [key: string]: RuntimeType } = {
-  "dotnet6": RuntimeType.DOTNET,
-  "dotnet8": RuntimeType.DOTNET,
-  "dotnet10": RuntimeType.DOTNET,
-  "nodejs12.x": RuntimeType.NODE,
-  "nodejs14.x": RuntimeType.NODE,
-  "nodejs16.x": RuntimeType.NODE,
-  "nodejs18.x": RuntimeType.NODE,
-  "nodejs20.x": RuntimeType.NODE,
-  "nodejs22.x": RuntimeType.NODE,
-  "nodejs24.x": RuntimeType.NODE,
-  "python3.6": RuntimeType.PYTHON,
-  "python3.7": RuntimeType.PYTHON,
-  "python3.8": RuntimeType.PYTHON,
-  "python3.9": RuntimeType.PYTHON,
-  "python3.10": RuntimeType.PYTHON,
-  "python3.11": RuntimeType.PYTHON,
-  "python3.12": RuntimeType.PYTHON,
-  "python3.13": RuntimeType.PYTHON,
-  "python3.14": RuntimeType.PYTHON,
-  "java8.al2": RuntimeType.JAVA,
-  "java11": RuntimeType.JAVA,
-  "java17": RuntimeType.JAVA,
-  "java21": RuntimeType.JAVA,
-  "java25": RuntimeType.JAVA,
-  "provided": RuntimeType.CUSTOM,
-  "provided.al2": RuntimeType.CUSTOM,
-  "provided.al2023": RuntimeType.CUSTOM,
-  "ruby3.2": RuntimeType.RUBY,
-  "ruby3.3": RuntimeType.RUBY,
-  "ruby3.4": RuntimeType.RUBY,
-  "ruby4.0": RuntimeType.RUBY,
-};
+export const runtimeLookup: { [key: string]: RuntimeType } = Object.fromEntries(
+  layerRuntimeCatalog.flatMap((runtime) =>
+    "runtimeType" in runtime ? [[runtime.runtime, runtimeTypeLookup[runtime.runtimeType]]] : [],
+  ),
+);
 
-export const runtimeToLayerName: { [key: string]: string } = {
-  "dotnet6": "dd-trace-dotnet",
-  "dotnet8": "dd-trace-dotnet",
-  "dotnet10": "dd-trace-dotnet",
-  "nodejs12.x": "Datadog-Node12-x",
-  "nodejs14.x": "Datadog-Node14-x",
-  "nodejs16.x": "Datadog-Node16-x",
-  "nodejs18.x": "Datadog-Node18-x",
-  "nodejs20.x": "Datadog-Node20-x",
-  "nodejs22.x": "Datadog-Node22-x",
-  "nodejs24.x": "Datadog-Node24-x",
-  "python3.6": "Datadog-Python36",
-  "python3.7": "Datadog-Python37",
-  "python3.8": "Datadog-Python38",
-  "python3.9": "Datadog-Python39",
-  "python3.10": "Datadog-Python310",
-  "python3.11": "Datadog-Python311",
-  "python3.12": "Datadog-Python312",
-  "python3.13": "Datadog-Python313",
-  "python3.14": "Datadog-Python314",
-  "java8.al2": "dd-trace-java",
-  "java11": "dd-trace-java",
-  "java17": "dd-trace-java",
-  "java21": "dd-trace-java",
-  "java25": "dd-trace-java",
-  "ruby32": "Datadog-Ruby3-2",
-  "ruby33": "Datadog-Ruby3-3",
-  "ruby34": "Datadog-Ruby3-4",
-  "ruby40": "Datadog-Ruby4-0",
-};
+export const runtimeToLayerName: { [key: string]: { x86_64: string; arm64: string } } = Object.fromEntries(
+  layerRuntimeCatalog.flatMap((runtime) => ("layerNames" in runtime ? [[runtime.runtime, runtime.layerNames]] : [])),
+);
 
 export const govCloudRegions: ReadonlyArray<string> = ["us-gov-east-1", "us-gov-west-1"];
 
